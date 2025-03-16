5594cb14314e5e178bed2dfb8abb8e71f612f55a
Andreagazy <andreagazyia@gmail.com>
2025-03-16 20:59:40 +0700

    API Letter Format

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { LetterModule } from './modules/letter/letter.module';
+import { LetterFormatModule } from './modules/letterFormat/letterFormat.module';
 
 @Module({
-  imports: [UserModule, LetterModule],
+  imports: [UserModule, LetterModule, LetterFormatModule],
 })
 export class AppModule {}
