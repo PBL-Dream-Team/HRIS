6be9dc2ccfddbe6ec563ff10b1e83bb0e8112c29
radeonaru <radeonaru@gmail.com>
2025-03-17 04:35:04 +0700

    fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { PaymentModule } from './modules/payment/payment.module';
 import { EmployeeModule } from './modules/employee/employee.module';
 import { SalaryModule } from './modules/salary/salary.module';
 import { LetterModule } from './modules/letter/letter.module';
 import { LetterFormatModule } from './modules/letterFormat/letterFormat.module';
+import configuration from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load:[configuration],
-      isGlobal:true}),
+      load: [configuration],
+      isGlobal: true,
+    }),
     PrismaModule,
     UserModule,
     PaymentModule,
     EmployeeModule,
     SalaryModule,
     LetterModule,
-    LetterFormatModule
+    LetterFormatModule,
   ],
-
-
+})
 export class AppModule {}
