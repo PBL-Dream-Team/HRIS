3274e14974837cd6c39a28b410229164045f5733
armand010 <andikaputrasamawa@gmail.com>
2025-05-09 00:33:47 +0700

    feat: Dashboard Admin

--- apps/frontend/src/app/user/[id]/admin/dashboard/page.tsx
+++ apps/frontend/src/app/user/[id]/admin/dashboard/page.tsx
@@ -1,33 +1,61 @@
+"use client";
 import { AppSidebar } from '@/components/app-sidebar';
+import { Input } from '@/components/ui/input';
+import { Bell, ExternalLink } from 'lucide-react';
+import Link from 'next/link';
+import { NavUser } from '@/components/nav-user';
+import { Separator } from '@/components/ui/separator';
+
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbList,
   BreadcrumbPage,
 } from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
+
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar';
 
-import { Input } from '@/components/ui/input';
-import { Bell, UserCheck } from 'lucide-react';
-import { NavUser } from '@/components/nav-user';
 
-import { User, 
+import { 
+  Bar, 
+  BarChart, 
+  CartesianGrid, 
+  LabelList, 
+  XAxis, 
+  YAxis, 
+  Pie, 
+  PieChart, 
+  Tooltip, 
+  ResponsiveContainer, 
+  Cell
+} from "recharts";
+
+import { 
+  User, 
   UserPlus, 
-  UserRoundCheck, 
+  UserCheck, 
   UserMinus 
 } from 'lucide-react';
 
-import { Card,
+import { 
+  Card,
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle
+  CardTitle,
+  CardContent
  } from '@/components/ui/card';
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
 
 import {
   DropdownMenu,
@@ -38,6 +66,25 @@ import {
   DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu';
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+
 const data = {
   user: {
     name: 'shadcn',
@@ -45,6 +92,53 @@ const data = {
     avatar: '/avatars/shadcn.jpg',
   },
 };
+
+const employeeDataStatistics = [
+  { statistic: "New", total: 200 },
+  { statistic: "Active", total: 186 },
+  { statistic: "Resign", total: 50 }
+];
+
+const employeeDataStatus = [
+  { status: "Permanent", total: 200 },
+  { status: "Trial", total: 186 },
+  { status: "Contract", total: 50 },
+  { status: "Intern", total: 50 }
+];
+
+const attendancesData = [
+  { attendance: "onTime", total: 200, fill: "hsl(var(--chart-1))" },
+  { attendance: "late", total: 186, fill: "hsl(var(--chart-2))" },
+  { attendance: "leave", total: 50, fill: "hsl(var(--chart-3))" },
+];
+
+const chartConfig = {
+  total: {
+    label: "Total",
+    color: "#000000",
+  },
+  onTime: {
+    label: "On Time",
+    color: "#257047",
+  },
+  late: {
+    label: "Late",
+    color: "#FFAB00",
+  },
+  leave: {
+    label: "Leave",
+    color: "#C11106",
+  },
+} satisfies ChartConfig
+
+const attendancesTableData = [
+  { 
+    id: 1, 
+    name: "John Doe",
+    status: "On Time",
+    clockIn: "08:00",
+  },
+]
 
 export default function Page() {
   return (
@@ -180,7 +274,279 @@ export default function Page() {
               </CardFooter>
             </Card>
           </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          <div className="grid auto-rows-min gap-4 md:grid-cols-2 sm:grid-cols-1 bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+            <Card>
+              <CardHeader className='relative pb-4'>
+                <div className='flex items-center gap-2'>
+                  <div>
+                    <CardTitle>Employee Statistics</CardTitle>
+                    <CardDescription>Current Number of Employees</CardDescription>
+                  </div>
+                  <div className='absolute right-4 top-4 pr-1'>
+                    <Select>
+                      <SelectTrigger className="w-[130px]">
+                        <SelectValue placeholder="Select Month" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="january">January</SelectItem>
+                          <SelectItem value="february">February</SelectItem>
+                          <SelectItem value="march">March</SelectItem>
+                          <SelectItem value="april">April</SelectItem>
+                          <SelectItem value="may">May</SelectItem>
+                          <SelectItem value="june">June</SelectItem>
+                          <SelectItem value="july">July</SelectItem>
+                          <SelectItem value="august">August</SelectItem>
+                          <SelectItem value="september">September</SelectItem>
+                          <SelectItem value="october">October</SelectItem>
+                          <SelectItem value="november">November</SelectItem>
+                          <SelectItem value="december">December</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <ResponsiveContainer width="100%" height={250}>
+                  <BarChart data={employeeDataStatistics} margin={{ top: 15, right: 0, left: -25, bottom: 5 }}>
+                    <CartesianGrid vertical={false} strokeDasharray="3 3" />
+                    <XAxis dataKey="statistic" tickLine={false}/>
+                    <YAxis  
+                      axisLine={false} 
+                      tickLine={false}
+                      domain={[0, 200]}
+                    />
+                    <Tooltip
+                      cursor={false}
+                      content={({ active, payload }) => {
+                        if (active && payload && payload.length) {
+                          return (
+                            <div className="bg-white p-2 shadow rounded text-sm">
+                              <p> Total = {payload[0].value}</p>
+                            </div>
+                          );
+                        }
+                        return null;
+                      }}
+                    />
+                    <Bar dataKey="total" fill="#1E3A5F" radius={[10, 10, 0, 0]}>
+                    </Bar>
+                  </BarChart>
+                </ResponsiveContainer>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className='relative pb-4'>
+                <div className='flex items-center gap-2'>
+                  <div>
+                    <CardTitle>Employee Status</CardTitle>
+                    <CardDescription>Current Status of Employees</CardDescription>
+                  </div>
+                  <div className='absolute right-4 top-4 pr-1'>
+                    <Select>
+                      <SelectTrigger className="w-[130px]">
+                        <SelectValue placeholder="Select Month" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="january">January</SelectItem>
+                          <SelectItem value="february">February</SelectItem>
+                          <SelectItem value="march">March</SelectItem>
+                          <SelectItem value="april">April</SelectItem>
+                          <SelectItem value="may">May</SelectItem>
+                          <SelectItem value="june">June</SelectItem>
+                          <SelectItem value="july">July</SelectItem>
+                          <SelectItem value="august">August</SelectItem>
+                          <SelectItem value="september">September</SelectItem>
+                          <SelectItem value="october">October</SelectItem>
+                          <SelectItem value="november">November</SelectItem>
+                          <SelectItem value="december">December</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <ResponsiveContainer width="100%" height={250}>
+                  <BarChart data={employeeDataStatus} margin={{ top: 15, right: 0, left: -25, bottom: 5 }}>
+                    <CartesianGrid vertical={false} strokeDasharray="3 3" />
+                    <XAxis dataKey="statistic" tickLine={false}/>
+                    <YAxis  
+                      axisLine={false} 
+                      tickLine={false}
+                      domain={[0, 200]}
+                    />
+                    <Tooltip
+                      cursor={false}
+                      content={({ active, payload }) => {
+                        if (active && payload && payload.length) {
+                          return (
+                            <div className="bg-white p-2 shadow rounded text-sm">
+                              <p> Total = {payload[0].value}</p>
+                            </div>
+                          );
+                        }
+                        return null;
+                      }}
+                    />
+                    <Bar dataKey="total" fill="#1E3A5F" radius={[10, 10, 0, 0]}>
+                    </Bar>
+                  </BarChart>
+                </ResponsiveContainer>
+              </CardContent>
+            </Card>
+            <Card className='@container/card'>
+              <div className='grid grid-cols-10 h-full items-center p-0'>
+                <div className='col-span-4 p-0'>
+                  <CardContent className='flex-1 pt-0 pb-0 pl-6 pr-0 h-full'> 
+                    <ChartContainer
+                      config={chartConfig}
+                      className="mx-auto w-full h-full min-h-[300px] [&_.recharts-text]:fill-background"
+                    >
+                      <PieChart
+                        width={300}
+                        height={300}
+                        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+                      >
+                        <ChartTooltip
+                          content={<ChartTooltipContent nameKey="total" hideLabel />}
+                        />
+                        <Pie 
+                          data={attendancesData} 
+                          dataKey="total"
+                          outerRadius={80} 
+                        >
+                          {attendancesData.map((entry, index) => (
+                            <Cell
+                              key={`cell-${index}`}
+                              fill={chartConfig[entry.attendance as keyof typeof chartConfig]?.color}
+                            />
+                          ))}
+                          <LabelList
+                            dataKey="attendance"
+                            className="fill-background"
+                            stroke="none"
+                            fontSize={12}
+                            formatter={(value: keyof typeof chartConfig) =>
+                              chartConfig[value]?.label
+                            }
+                          />
+                        </Pie>
+                      </PieChart>
+                    </ChartContainer>
+                  </CardContent>
+                </div>
+                <div className='col-span-6'>
+                  <CardContent className="flex flex-col gap-4 pt-0 pb-0 pl-6 pr-6">
+                    <div className="flex justify-between text-sm">
+                      <span className="font-medium text-muted-foreground">Statistic</span>
+                      <span className="text-muted-foreground">Today</span>
+                    </div>
+                    <div className="flex justify-between items-center text-sm">
+                      <span className="text-muted-foreground">Attendance</span>
+                      <span className="text-muted-foreground">02 May 2025</span>
+                    </div>
+                    <hr />
+                    <div className="flex flex-col gap-3">
+                      {attendancesData.slice(0, 4).map((item, index) => (
+                        <div key={item.attendance} className="flex justify-between items-center">
+                          <div className="flex items-center gap-2">
+                            <span
+                              className="w-3 h-3 rounded-full flex-shrink-0"
+                              style={{ 
+                                backgroundColor: chartConfig[item.attendance as keyof typeof chartConfig]?.color
+                              }}
+                            ></span>
+                            <span className="text-sm text-muted-foreground font-medium">
+                              {chartConfig[item.attendance as keyof typeof chartConfig]?.label}
+                            </span>
+                          </div>
+                          <span className="text-sm font-medium">{item.total}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </CardContent>
+                </div>
+              </div>
+            </Card>
+            <Card>
+              <CardHeader className='relative'>
+                <div>
+                  <div className='flex items-center gap-2 pb-4'>
+                    <h1>Attendance</h1>
+                    <div className='absolute right-4 top-4 pr-1 flex items-center'>
+                      <Select>
+                        <SelectTrigger className="w-[130px]">
+                          <SelectValue placeholder="Select Month" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectItem value="january">January</SelectItem>
+                            <SelectItem value="february">February</SelectItem>
+                            <SelectItem value="march">March</SelectItem>
+                            <SelectItem value="april">April</SelectItem>
+                            <SelectItem value="may">May</SelectItem>
+                            <SelectItem value="june">June</SelectItem>
+                            <SelectItem value="july">July</SelectItem>
+                            <SelectItem value="august">August</SelectItem>
+                            <SelectItem value="september">September</SelectItem>
+                            <SelectItem value="october">October</SelectItem>
+                            <SelectItem value="november">November</SelectItem>
+                            <SelectItem value="december">December</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                      <a href="/user/[id]/employee/checkclock">
+                        <button>
+                          <ExternalLink className="h-7 w-7 ml-2" />
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-3">
+                      {attendancesData.slice(0, 4).map((item, index) => (
+                        <div key={item.attendance} className="flex gap-2 items-center after:content-['|'] last:after:hidden">
+                          <span
+                            className="w-3 h-3 rounded-full flex-shrink-0"
+                            style={{
+                              backgroundColor: chartConfig[item.attendance as keyof typeof chartConfig]?.color
+                            }}
+                          ></span>
+                          <span className="text-sm font-medium">{item.total}</span>
+                          <span className="text-sm font-medium">
+                            {chartConfig[item.attendance as keyof typeof chartConfig]?.label}
+                          </span>
+                        </div>
+                      ))}
+                    </div>
+                </div>
+              </CardHeader>  
+              <CardContent>
+                <Table>
+                  <TableHeader className='bg-[#1E3A5F] text-white'>
+                    <TableRow>
+                      <TableHead>No</TableHead>
+                      <TableHead>Name</TableHead>
+                      <TableHead>Status</TableHead>
+                      <TableHead>Clock In</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {attendancesTableData.map((at) => (
+                      <TableRow key={at.id}>
+                        <TableCell>{at.id}</TableCell>
+                        <TableCell>{at.name}</TableCell>
+                        <TableCell>{at.status}</TableCell>
+                        <TableCell>{at.clockIn}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardContent>                
+            </Card>
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
