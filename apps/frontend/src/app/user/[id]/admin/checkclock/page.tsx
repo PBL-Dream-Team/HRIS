07c410fb1828cc423eabf9ebcaff4468c6136d91
FaridFitriansahAlfarizi <faridfitriansahalfarizi@gmail.com>
2025-05-12 13:20:48 +0700

    feat: add checkclock

--- apps/frontend/src/app/user/[id]/admin/checkclock/page.tsx
+++ apps/frontend/src/app/user/[id]/admin/checkclock/page.tsx
@@ -172,15 +172,129 @@ export default function Page() {
                   </Button>
                   <Dialog>
                     <DialogTrigger asChild>
-                      <Button className="w-full md:w-auto">
-                        <Plus className="h-4 w-4 mr-1" /> Tambah Data
+                      <Button>
+                        <IoMdAdd /> Add Check Clock
                       </Button>
                     </DialogTrigger>
-
-                    <DialogContent className="max-w-5xl max-h-[90vh] overflow-y-auto">
+                    <DialogContent className="max-w-4xl">
                       <DialogHeader>
-                        <DialogTitle>Add New Employee</DialogTitle>
+                        <DialogTitle>Add Check Clock</DialogTitle>
                       </DialogHeader>
+                      <form>
+                        <div className="flex flex-col md:flex-row gap-x-6">
+                          {/* Left Form*/}
+                          <div className="w-full md:w-1/2 mb-4 md:mb-0 space-y-4">
+                            <Label htmlFor="letterType">Absence Type</Label>
+                            <div className="mt-2">
+                              <Select>
+                                <SelectTrigger className="w-full">
+                                  <SelectValue placeholder="-Choose Absence Type-" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                  <SelectItem value="absent">Absent</SelectItem>
+                                  <SelectItem value="ontime">
+                                    On Time
+                                  </SelectItem>
+                                  <SelectItem value="late">Late</SelectItem>
+                                </SelectContent>
+                              </Select>
+                            </div>
+                            <Label htmlFor="letterPicture">
+                              Upload Evidence Picture
+                            </Label>
+                            <div className="mt-2 relative w-full aspect-[20/19] border rounded-lg shadow-sm flex items-center justify-center hover:bg-gray-100 transition cursor-pointer">
+                              {previewUrl ? (
+                                <img
+                                  src={previewUrl}
+                                  alt="Preview"
+                                  className="w-full h-full object-cover rounded-lg"
+                                />
+                              ) : (
+                                <span className="flex flex-col items-center justify-center text-black text-sm">
+                                  <MdImage className="text-3xl text-[#1E3A5F] mb-1" />
+                                  Click to upload
+                                </span>
+                              )}
+                              <Input
+                                id="letterPicture"
+                                type="file"
+                                accept="image/*"
+                                onChange={handleFileChange}
+                                className="absolute opacity-0 w-full h-full cursor-pointer"
+                              />
+                            </div>
+                          </div>
+
+                          {/* Form Right */}
+                          <div className="w-full md:w-1/2 space-y-4">
+                            <Label htmlFor="location">Work Location</Label>
+                            <div className="mt-2">
+                              <Select>
+                                <SelectTrigger className="w-full">
+                                  <SelectValue placeholder="-Choose Location-" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                  <SelectItem value="WFO">WFO</SelectItem>
+                                  <SelectItem value="WFH">WFH</SelectItem>
+                                  <SelectItem value="WFA">WFA</SelectItem>
+                                </SelectContent>
+                              </Select>
+                            </div>
+                            <MapPicker
+                              onLocationSelect={handleLocationSelect}
+                            />
+
+                            <Label htmlFor="addressDetail">
+                              Address Detail
+                            </Label>
+                            <div className="mt-2">
+                              <Input
+                                id="addressDetail"
+                                type="text"
+                                placeholder="Enter address detail"
+                                value={addressDetail}
+                                onChange={(e) =>
+                                  setAddressDetail(e.target.value)
+                                }
+                              />
+                            </div>
+                            <div className="flex flex-col md:flex-row gap-x-6">
+                              <div className="w-full md:w-1/2 space-y-4">
+                                <Label htmlFor="lat">Lat</Label>
+                                <div className="mt-2">
+                                  <Input
+                                    id="lat"
+                                    type="text"
+                                    value={lat}
+                                    onChange={(e) => setLat(e.target.value)}
+                                    readOnly
+                                    className="bg-gray-100"
+                                  />
+                                </div>
+                              </div>
+                              <div className="w-full md:w-1/2 space-y-4">
+                                <Label htmlFor="long">Long</Label>
+                                <div className="mt-2">
+                                  <Input
+                                    id="long"
+                                    type="text"
+                                    value={lng}
+                                    onChange={(e) => setLng(e.target.value)}
+                                    readOnly
+                                    className="bg-gray-100"
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <DialogFooter className="gap-2 sm:justify-end mt-4">
+                          <Button type="submit" className="w-20">
+                            Submit
+                          </Button>
+                        </DialogFooter>
+                      </form>
                     </DialogContent>
                   </Dialog>
                 </div>
