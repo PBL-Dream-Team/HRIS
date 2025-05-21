55c7c78164d2fa1956bf9c8e169d8add56b89031
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-05-21 13:22:06 +0700

    fix: input selectors in signin tests

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   distDir: '../../dist/apps/frontend',
-  allowedDevOrigins: ['http://34.50.86.184:3000']
 };
 
 export default nextConfig;
