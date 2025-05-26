b364f4ef4f1fc11fa66156153688fcea3ae5b1a9
Andreagazy <andreagazyia@gmail.com>
2025-05-26 20:05:27 +0700

    HR Dashboard (10%) - Need Data

--- apps/backend/src/modules/Employee/employee.controller.ts
+++ apps/backend/src/modules/Employee/employee.controller.ts
@@ -1,26 +1,26 @@
-import {Controller,Get,Post,Body,Param,Delete,Patch, UseGuards, Query, UploadedFile, UseInterceptors, UploadedFiles} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, UseGuards, Query, UploadedFile, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { createEmployeeDto } from './dtos/createEmployee.dto';
 import { editEmployeeDto } from './dtos/editEmployee.dto';
 import { JwtGuard, SubscriptionGuard } from '../Auth/guard';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { UploadExtensionInterceptor } from '../../multer/image_upload.interceptor';
-import { FileInterceptor, FilesInterceptor} from '@nestjs/platform-express';
-import {} from 'multer';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { } from 'multer';
 
 @ApiTags("employee")
 @UseGuards(JwtGuard, SubscriptionGuard)
 @Controller('api/employee')
 export class EmployeeController {
-  constructor(private readonly employeeService: EmployeeService) {}
+  constructor(private readonly employeeService: EmployeeService) { }
 
   @Post()
-  @ApiBody({type:createEmployeeDto})
+  @ApiBody({ type: createEmployeeDto })
   @UseInterceptors(
-    FileInterceptor('file',{
-      limits: { fileSize: 50 * 1024 * 1024},
+    FileInterceptor('file', {
+      limits: { fileSize: 50 * 1024 * 1024 },
     }),
-    new UploadExtensionInterceptor(['jpg','jpeg','png'])
+    new UploadExtensionInterceptor(['jpg', 'jpeg', 'png'])
   )
   createEmployee(
     @Body() createEmployeeDto: createEmployeeDto,
@@ -30,12 +30,12 @@ export class EmployeeController {
   }
 
   @Get()
-  getEmployees(@Query() query: Record<string,any>) {
-      if(Object.keys(query).length >0){
-          return this.employeeService.findFilters(query);
-      }
-      return this.employeeService.getEmployees();
+  getEmployees(@Query() query: Record<string, any>) {
+    if (Object.keys(query).length > 0) {
+      return this.employeeService.findFilters(query);
     }
+    return this.employeeService.getEmployees();
+  }
 
   @Get(':id')
   getEmployee(@Param('id') employeeId: string) {
@@ -43,12 +43,12 @@ export class EmployeeController {
   }
 
   @Patch(':id')
-  @ApiBody({type:editEmployeeDto})
+  @ApiBody({ type: editEmployeeDto })
   @UseInterceptors(
-    FileInterceptor('file',{
-      limits: { fileSize: 50 * 1024 * 1024},
+    FileInterceptor('file', {
+      limits: { fileSize: 50 * 1024 * 1024 },
     }),
-    new UploadExtensionInterceptor(['jpg','jpeg','png'])
+    new UploadExtensionInterceptor(['jpg', 'jpeg', 'png'])
   )
   updateEmployee(
     @Param('id') employeeId: string,
@@ -62,4 +62,16 @@ export class EmployeeController {
   deleteEmployee(@Param('id') employeeId: string) {
     return this.employeeService.deleteEmployee(employeeId);
   }
+
+  @Get('count/:companyId')
+  async getEmployeeCount(@Param('companyId') companyId: string) {
+    return this.employeeService.countEmployees(companyId);
+  }
+
+  @Get('status-summary/:companyId')
+  async getEmployeeStatusByContract(@Param('companyId') companyId: string) {
+    return this.employeeService.getStatusSummary(companyId);
+  }
+
+
 }
