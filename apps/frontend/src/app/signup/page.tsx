49471ae90f6e6d6b1200add7bc87ac5fb9c591fd
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-05-29 21:29:50 +0700

    feat: seperating Terms of Use into components

--- apps/frontend/src/app/signup/page.tsx
+++ apps/frontend/src/app/signup/page.tsx
@@ -10,6 +10,7 @@ import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Label } from '@/components/ui/label';
 import { useRouter } from 'next/navigation';
+import TermsOfUse from '@/components/terms-of-use';
 import {
   Dialog,
   DialogContent,
@@ -190,93 +191,12 @@ export default function HrSignUpPage() {
                 className="data-[state=checked]:bg-[#1E3A5F] data-[state=checked]:border-[#1E3A5F]"
               />{' '}
               <span>I agree with the</span>
-              <Dialog>
+                            <Dialog>
                 <DialogTrigger asChild>
-                  <button
-                    type="button"
-                    className="text-sm text-gray-700 underline"
-                  >
-                    terms of use of HRIS
-                  </button>
+                  <button type="button" className="text-sm text-gray-700 underline">terms of use of HRIS</button>
                 </DialogTrigger>
                 <DialogContent className="max-h-[80vh] overflow-y-scroll">
-                  <DialogHeader>
-                    <DialogTitle>Terms of Use</DialogTitle>
-                    <DialogDescription className="text-gray-600">
-                      Ini adalah isi Terms of Use HRIS.
-                      <br />
-                      <br />
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Molestias ut facere blanditiis. Dolor quidem accusamus
-                      quos facilis? Maxime, dolorum! Nam quos ex voluptates
-                      obcaecati, provident earum assumenda dolore, ut sequi
-                      repudiandae ipsam suscipit! Repellendus ab temporibus
-                      quam. Cum nihil voluptates vero, sequi maxime, reiciendis
-                      natus eveniet quam itaque laborum doloribus a qui vel id
-                      odio nobis expedita! Enim, culpa quod iste quidem aut
-                      iusto aliquam nobis ullam veritatis libero mollitia.
-                      Facere illo odit eaque consectetur quidem aperiam
-                      voluptatibus molestiae, totam sint illum dolores vero
-                      animi neque? Ratione adipisci, dicta impedit neque
-                      expedita doloremque sequi consectetur similique vel
-                      laudantium minus incidunt. Porro mollitia minima, eius
-                      iure, dicta a, accusantium neque unde ducimus quas fugiat
-                      ullam vitae! Perspiciatis dicta soluta est cum officia
-                      illo ab facere, suscipit fugit illum. Labore, explicabo
-                      deserunt? Blanditiis beatae, fugiat quas reiciendis quo
-                      nisi, nostrum nobis consequatur ducimus iste a veritatis
-                      corrupti provident alias quod tempora laborum veniam
-                      cupiditate reprehenderit possimus dicta! Nemo, tempora
-                      voluptas ex facere a nobis voluptatem quas vel ab
-                      temporibus unde nihil laudantium tenetur ullam eius illum
-                      iure, quis veritatis fugiat, voluptates commodi autem
-                      dolores. Labore cum, nisi necessitatibus est, ipsam
-                      veritatis porro aspernatur sunt eligendi similique
-                      inventore quisquam itaque culpa odit molestias. Architecto
-                      laboriosam tenetur, molestiae inventore aut delectus
-                      pariatur repellendus, ad corporis veniam illo voluptatum,
-                      repellat facere. Porro perspiciatis pariatur velit
-                      deserunt ullam, amet autem molestias blanditiis ratione,
-                      adipisci architecto? Reprehenderit nesciunt incidunt
-                      voluptatem debitis vitae odio a doloremque temporibus
-                      obcaecati odit molestias aliquid, officiis quam
-                      consectetur ducimus dicta aliquam. Libero possimus numquam
-                      qui, itaque necessitatibus ducimus quae impedit eum animi
-                      cupiditate ab sequi? Necessitatibus exercitationem at,
-                      repellat vitae et consequatur eius rem quaerat magni quasi
-                      voluptatum mollitia deserunt praesentium, maiores fugit
-                      nihil voluptates vero dolorem ipsa corrupti totam
-                      possimus! Culpa odit alias fugit, in accusantium dolores,
-                      laborum ea praesentium nesciunt ex reiciendis impedit
-                      doloribus. Sapiente, ipsa. Accusamus explicabo blanditiis
-                      pariatur dolor. Similique ab reiciendis neque nesciunt
-                      temporibus quia, beatae facere ex corporis omnis maxime
-                      minima libero aliquid hic tenetur, dolore molestias
-                      cupiditate eos? Provident aliquid, natus voluptate iure
-                      accusamus aspernatur, libero molestiae eveniet unde earum
-                      eius perspiciatis. Sunt, magnam quaerat! In ipsum a
-                      corrupti similique, deserunt tempore, harum id, deleniti
-                      nisi nulla adipisci laborum laboriosam quaerat porro.
-                      Laboriosam aliquam eum itaque similique delectus suscipit
-                      necessitatibus sapiente illum culpa aspernatur a magni
-                      vero eaque totam quibusdam rem, dolore fugiat esse modi
-                      consequuntur corrupti! Dolor ad mollitia deserunt a
-                      tempore expedita accusamus voluptas unde laborum commodi
-                      beatae laudantium, deleniti eos aliquam. Facere a nam
-                      temporibus voluptatibus. Autem assumenda eaque mollitia,
-                      vitae pariatur perferendis maxime in velit tempore minus
-                      deleniti consequatur quae blanditiis tempora nam.
-                      Distinctio veritatis labore quos et quasi iusto voluptas
-                      sit, perspiciatis laboriosam, itaque maiores explicabo
-                      facere animi voluptatem ducimus cumque magni saepe, sint
-                      molestias maxime enim culpa temporibus. Laudantium et nisi
-                      cumque impedit. Rem, labore? Culpa dolores fuga harum
-                      blanditiis sed aliquam, quod voluptatem magni nisi magnam
-                      soluta a quibusdam quidem. Magnam, dicta rerum in, velit
-                      optio delectus accusamus est sed corrupti placeat pariatur
-                      sequi accusantium error at odit.
-                    </DialogDescription>
-                  </DialogHeader>
+                  <TermsOfUse />
                 </DialogContent>
               </Dialog>
             </div>
